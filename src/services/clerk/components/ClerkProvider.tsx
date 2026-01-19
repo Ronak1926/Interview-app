5e0bfbe7f7a9063c611cc4c0409ea83ac28c555a
Ronak <ronaksojitra1926@gmail.com>
2026-01-19 16:29:32 +0530

    basic signup is completed

---
+++ src/services/clerk/components/ClerkProvider.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from "react";
+import { ClerkProvider as OriginalClerkProvider } from "@clerk/nextjs";
+export function ClerkProvider({ children }: { children: ReactNode }) {
+  return <OriginalClerkProvider>{children}</OriginalClerkProvider>
+}
