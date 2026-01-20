28d64546ecca1a2d31d582687e88e8096d83c13f
Ronak <ronaksojitra1926@gmail.com>
2026-01-20 15:13:14 +0530

    allow the api route so webhook can see it to insert into the db

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,43 +1,45 @@
-import arcjet, { detectBot, shield, slidingWindow } from '@arcjet/next'
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { env } from './data/env/server'
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { env } from "./data/env/server";
 
 const aj = arcjet({
-    // Todo: add type safety for env
-    key: env.ARCJET_KEY!,
-    rules: [
-        shield({mode: "LIVE"}),
-        detectBot({
-            mode: "LIVE",
-            allow: ['CATEGORY:SEARCH_ENGINE','CATEGORY:PREVIEW','CATEGORY:MONITOR'],
-        }),
-        slidingWindow({
-            mode: "LIVE",
-            interval: "1m",
-            max: 200,
-        })
-    ]
-})
+  // Todo: add type safety for env
+  key: env.ARCJET_KEY!,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:PREVIEW", "CATEGORY:MONITOR"],
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      interval: "1m",
+      max: 200,
+    }),
+  ],
+});
 
 const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)','/'
-])
-export default clerkMiddleware(async (auth, req)=> {
-    const decision = await aj.protect(req)
+  "/sign-in(.*)",
+  "/",
+  "/api/webhooks(.*)",
+]);
+export default clerkMiddleware(async (auth, req) => {
+  const decision = await aj.protect(req);
 
-    if(decision.isDenied()){
-        return new Response(null, {status: 403})
-    }
-    if(!isPublicRoute(req)){
-        await auth.protect()
-    }
-})
+  if (decision.isDenied()) {
+    return new Response(null, { status: 403 });
+  }
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
