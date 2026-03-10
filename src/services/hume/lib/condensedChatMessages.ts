be12338f2d097fe3c3ae3676ae5782abc81561ec
Ronak <ronaksojitra1926@gmail.com>
2026-03-10 12:45:50 +0530

    condesed message comp and chatmessage func

---
+++ src/services/hume/lib/condensedChatMessages.ts
@@ -0,0 +1,48 @@
+import { Hume } from "hume"
+import { ConnectionMessage } from "@humeai/voice-react"
+
+type JsonMessage = Hume.empathicVoice.JsonMessage
+type ReturnChatEvent = Hume.empathicVoice.ReturnChatEvent
+type Message = JsonMessage | ConnectionMessage | ReturnChatEvent
+
+export function condensedChatMessages(messages: Message[]) {
+    return messages.reduce((acc, message) => {
+        const data = getChatEventData(message) ?? getJsonMessageData(message)
+        if (data == null || data.content == null) {
+            return acc
+        }
+        const lastMessage = acc.at(-1)
+        if (lastMessage == null) {
+            acc.push({ isUser: data.isUser, content: [data.content] })
+        } else if (lastMessage.isUser === data.isUser) {
+            lastMessage.content.push(data.content)
+        } else {
+            acc.push({ isUser: data.isUser, content: [data.content] })
+        }
+        return acc
+    }, [] as { isUser: boolean; content: string[] }[])
+}
+
+
+function getJsonMessageData(message: Message) {
+    if (message.type !== "user_message" && message.type !== "assistant_message") {
+        return null
+    }
+
+    return {
+        isUser: message.type === "user_message",
+        content: message.message.content
+    }
+}
+
+
+function getChatEventData(message: Message) {
+    if (message.type !== "USER_MESSAGE" && message.type !== "AGENT_MESSAGE") {
+        return null
+    }
+
+    return {
+        isUser: message.type === "USER_MESSAGE",
+        content: message.messageText
+    }
+}
