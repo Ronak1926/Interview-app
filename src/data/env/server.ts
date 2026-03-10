42119c67eb7c346c48a96c7d83239a9fb416d01e
Ronak <ronaksojitra1926@gmail.com>
2026-03-10 12:41:19 +0530

    integarate: Hume ai for the voice

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -10,13 +10,15 @@ export const env = createEnv({
         DB_NAME: z.string().min(1),
         ARCJET_KEY: z.string().min(1),
         CLERK_SECRET_KEY: z.string().min(1),
+        HUME_API_KEY: z.string().min(1),
+        HUME_SECRET_KEY: z.string().min(1),
     },
     createFinalSchema: env => {
-        return z.object(env).transform(val=>{
-            const {DB_PASSWORD,DB_USER,DB_HOST,DB_PORT,DB_NAME,...rest} = val;
+        return z.object(env).transform(val => {
+            const { DB_PASSWORD, DB_USER, DB_HOST, DB_PORT, DB_NAME, ...rest } = val;
             return {
                 ...rest,
-                DATABASE_URL:`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+                DATABASE_URL: `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
             }
         })
     },
