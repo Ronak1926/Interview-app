80967515db9b294245edd1ac7a20b528ed0c4d2c
Ronak <ronaksojitra1926@gmail.com>
2026-03-11 23:15:16 +0530

    questions generation from ai

--- src/lib/dataCache.ts
+++ src/lib/dataCache.ts
@@ -1,4 +1,4 @@
-type CacheTag = "users" | "jobInfos" | "interviews" | "quetions";
+type CacheTag = "users" | "jobInfos" | "interviews" | "questions";
 
 export function getGlobalTag(tag: CacheTag) {
   return `global: ${tag}` as const;
@@ -9,7 +9,7 @@ export function getUserTag(tag: CacheTag, userId: string) {
 }
 
 export function getJobInfoTag(tag: CacheTag, jobInfoId: string) {
-  return `JobInfo ${jobInfoId}:${tag}` as const
+  return `JobInfo ${jobInfoId}:${tag}` as const;
 }
 
 export function getIdTag(tag: CacheTag, id: string) {
