f4ddd091aa8f4eed49de21eaa52bf036910b1143
Ronak <ronaksojitra1926@gmail.com>
2026-06-14 19:28:26 +0530

    switch to neon serverless db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   schema: "./src/drizzle/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: env.DATABASE_URL + "?sslmode=require",
+    ssl: true,
   },
 })
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,4 +1,7 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "@/data/env/server";
 import * as schema from "@/drizzle/schema";
-export const db = drizzle(env.DATABASE_URL, { schema });
+
+const sql = neon(env.DATABASE_URL);
+export const db = drizzle(sql, { schema });
